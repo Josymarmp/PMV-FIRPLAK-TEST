c24d01e1beb546bc10ba292b9a3af95c96f1c0b8
josy <yosifps@gmail.com>
2023-11-30 11:35:47 -0500

    se agrego config

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,18 +1,19 @@
 import app from "./app.js";
 import mysql from "mysql2/promise"; 
+import {PORT,DB_HOST,DB_USER,DB_NAME,DB_PASSWORD} from "./config.js"
 
     export const db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '2524169',
-      database: 'PMV',
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
     });
 
     if(db){
         console.log("MySQL connected");
     }
     
-    const port = 8000;
+    const port = PORT;
     app.listen(port, () => {
       console.log(`Server running on port: ${port}`);
     });
